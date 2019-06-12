a3a2bc13ce7d9ef827e5c5b6debd6b2f8b98110b
Ben Vinson <vinsonb@gmail.com>
2019-06-11 19:10:28 -0600

    Remove Rails 5.x deprecations

--- lib/prototype-rails/rendering.rb
+++ lib/prototype-rails/rendering.rb
@@ -1,13 +1,12 @@
 require 'action_view/helpers/rendering_helper'
 
 ActionView::Helpers::RenderingHelper.module_eval do
-  def render_with_update(options = {}, locals = {}, &block)
+  alias render_without_update render
+  def render(options = {}, locals = {}, &block)
     if options == :update
       update_page(&block)
     else
       render_without_update(options, locals, &block)
     end
   end
-  
-  alias_method_chain :render, :update
 end
